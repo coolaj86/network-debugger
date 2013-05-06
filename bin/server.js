f3f4d509de4145570ebc8d6135abe5cac07a8fa5
tigerbot <sethg@spotter360.com>
2013-05-06 10:59:10 -0600

    adding a proper argument parser

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,80 @@
 #!/usr/bin/env node
-/*jshint node:true laxcomma:true*/
+/*jshint node:true, laxcomma:true*/
 (function () {
   "use strict";
 
-  var port = process.argv[2] || 0
+  var path = require('path')
+    , ArgumentParser = require('argparse').ArgumentParser
     , init = require('../server').init
-    , path = require('path')
-    , app
     ;
 
-  function run() {
-    var server
+  require('colors');
+
+  function parseArgs() {
+    var parser
+      , args
+      ;
+
+    parser = new ArgumentParser({
+      version: require('../package.json').version,
+      addHelp: true,
+      description: 'Network listening tool'
+    });
+
+    parser.addArgument(
+      ['-p', '--port'],
+      {
+        action: 'store',
+        type: 'int',
+        dest: 'port',
+        metavar: '#',
+        help: 'The port on which the UI web pages are served'
+      }
+    );
+    parser.addArgument(
+      ['-l', '--log-dir'],
+      {
+        action: 'store',
+        type: 'string',
+        dest: 'logDir',
+        metavar: 'DIR',
+        help: 'The directory to place the log files'
+      }
+    );
+
+    // legacy support options
+    parser.addArgument(
+      ['legacyOpts'],
+      {
+        nargs: '*',
+        help: 'Legacy options from before we had a proper parser'
+      }
+    );
+
+    args = parser.parseArgs();
+
+    if (args.legacyOpts.length > 0) {
+      if (args.port !== null || args.logDir !== null) {
+        console.error('Do not mix floating arguments and options'.underline.red);
+        return args;
+      }
+      console.warn('floating arguments are now discouraged, please use -h or --help for details'.yellow);
+
+      if (!isNaN(args.legacyOpts[0])) {
+        args.port = args.legacyOpts[0];
+      }
+      if (args.legacyOpts[1]) {
+        args.logDir = args.legacyOpts[1];
+      }
+      delete args.legacyOpts;
+    }
+
+    return args;
+  }
+
+  function run(options) {
+    var app
+      , server
       ;
 
     function onListening() {
@@ -20,11 +84,20 @@
       console.log("Listening on http://%s:%d", addr.address, addr.port);
     }
 
-    server = app.listen(port, onListening);
+    if (!options.logDir) {
+      options.logDir = './netbug-logs';
+    }
+    options.logDir = path.resolve(options.logDir);
+
+    if (isNaN(options.port)) {
+      options.port = 0;
+    }
+
+    app = init(options.logDir);
+    server = app.listen(options.port, onListening);
   }
 
   if (require.main === module) {
-    app = init(process.argv[3] || path.resolve('netbug-logs'));
-    run();
+    run(parseArgs());
   }
 }());
